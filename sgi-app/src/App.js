5daf36c5f97525692657729fc818e93178e3579c
Cláudio Almeida <claudio.almeida@flex-am.com.br>
2019-11-29 16:44:04 -0400

    inicio do projeto

---
+++ sgi-app/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import FormLogin from '../src/views/base/FormLogin' ;
+import Routes from './base/routes';
+
+const App = () =>  <Routes /> ;
+  
+
+export default App;
