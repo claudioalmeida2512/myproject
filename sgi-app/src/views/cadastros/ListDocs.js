d4ecd73f4ef04e9b200e2842a0023340a7d75cf4
Cláudio Almeida <claudio.almeida@flex-am.com.br>
2019-12-05 15:33:10 -0400

    filtros  por campo de revisao

--- sgi-app/src/views/cadastros/ListDocs.js
+++ sgi-app/src/views/cadastros/ListDocs.js
@@ -1,47 +1,8 @@
 import React, { Component } from 'react';
 import api from '../../base/api';
-import TableForm from '../base/TableForm' ;
+import TableForm from '../base/TableForm';
 
-const columns = [
-    {
-        Header: "Status",
-        accessor: "doc_status"
-    },
-    {
-        Header: "Doc.Id",
-        accessor: "iddocumento"
-    },
-    {
-        Header: "Tipo",
-        accessor: "doc_tipo",
-       
-    },
-    {
-        Header: "Titulo",
-        accessor: "doc_titulo",
-        
-    },
-    {
-        Header: "Revisao",
-        accessor: "doc_rev"
-    },
-    {
-        Header: "Emissao",
-        accessor: "doc_emissao"
-    },
-    {
-        Header: "Fim",
-        accessor: "doc_final"
-    },
-    {
-        Header: "Usuario",
-        accessor: "doc_user"
-    },
-    {
-        Header: "Aprovadores",
-        accessor: "doc_aprov"
-    },
-];
+
 
 
 export default class FormList extends Component {
@@ -75,23 +36,100 @@ export default class FormList extends Component {
     }
 
     render() {
+        const columns = [
+            {
+                Header: "Status",
+                accessor: "doc_status"
+            },
+            {
+                Header: "Doc.Id",
+                accessor: "iddocumento"
+            },
+            {
+                Header: "Tipo",
+                accessor: "doc_tipo",
+                Filter: this.SelectColumnFilter ,
+                filter: 'includes',
+
+            },
+            {
+                Header: "Titulo",
+                accessor: "doc_titulo",
+
+            },
+            {
+                Header: "Revisao",
+                accessor: "doc_rev",
+                Filter: this.SelectColumnFilter ,
+                filter: 'includes',
+            },
+            {
+                Header: "Emissao",
+                accessor: "doc_emissao"
+            },
+            {
+                Header: "Fim",
+                accessor: "doc_final"
+            },
+            {
+                Header: "Usuario",
+                accessor: "doc_user"
+            },
+            {
+                Header: "Aprovadores",
+                accessor: "doc_aprov"
+            },
+        ];
         const data = this.state.listDocs;
         return (
             <div className="conteudoPrincipal">
                 <div className={this.state.colsList}>
                     <h1>Lista de Documentos</h1>
-                    
-                        <TableForm columns={columns} data={data}  />
-                   
+
+                    <TableForm columns={columns} data={data} />
+
 
                 </div>
             </div>
         );
     }
 
+
+
+    // This is a custom filter UI for selecting
+    // a unique option from a list
+    SelectColumnFilter = ({
+        column: { filterValue, setFilter, preFilteredRows, id },
+    }) => {
+        // Calculate the options for filtering
+        // using the preFilteredRows
+        const options = React.useMemo(() => {
+            const options = new Set()
+            preFilteredRows.forEach(row => {
+                options.add(row.values[id])
+            })
+            return [...options.values()]
+        }, [id, preFilteredRows])
+
+        // Render a multi-select box
+        return (
+            <select
+                value={filterValue}
+                onChange={e => {
+                    setFilter(e.target.value || undefined)
+                }}
+            >
+                <option value="">All</option>
+                {options.map((option, i) => (
+                    <option key={i} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        )
+    }
+
+    
+
 }
-
-
-
-
 
