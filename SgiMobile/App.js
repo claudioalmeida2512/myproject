929915a7e47fcb7c71f77ae541537928dba62fc3
Cláudio Almeida <claudio.almeida@flex-am.com.br>
2020-07-22 09:34:09 -0400

    teste

--- SgiMobile/App.js
+++ SgiMobile/App.js
@@ -1,47 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  Image,
-  Dimensions,
-  StatusBar,
-  FlatList,
-} from 'react-native';
+import App from './src/Home';
 
-const width = Dimensions.get('screen').width;
-
-const App: () => React$Node = () => {
-  const fotos = [
-    { id: 1, usuario: 'claudio' },
-    { id: 2, usuario: 'alberto' },
-    { id: 3, usuario: 'outros' }
-  ];
+export default function Main() {
   return (
-    <>
-      <FlatList style={{"marginTop": 20}}
-        keyExtractor={ item => item.id}
-        data={fotos}
-        renderItem={ ({ item }) =>
-          <View>
-            <Text>{item.usuario}</Text>
-            <Image source={require('./images/cnh.jpg')} style={{ width: width, height: width }} />
-          </View>
-        }
-      />
-    </>
+    <App/>
   );
-};
-
-
-export default App;
+}
